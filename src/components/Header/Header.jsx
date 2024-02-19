3692a2d51d32c59680198f3e10a0967cb0e15ace
pedroquintao <pedro.h.quintao@hotmail.com>
2024-02-19 19:52:39 -0300

    Vite foi substituido por cra porque o emotion não funcionou corretamento com o vite

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,23 @@
+import { Container, Row, Col } from 'react-grid-system'
+import moneyIcon from './money.png'
+// import styled from '@emotion/styled'
+
+// const StylizedMoneyIcon = styled.img`
+    
+// `
+
 export const Header = () => {
-    return <h1>HEADER!</h1>
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h1>teste</h1>
+                </Col>
+                <Col>
+                    <h1>HEADER!</h1>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
+
--- src/components/Theme/Theme.jsx
+++ src/components/Theme/Theme.jsx
@@ -2,7 +2,18 @@ import { ThemeProvider } from '@emotion/react'
 
 const theme = {
     colours: {
+        black:'#000',
+        white: '#FFF',
 
+        dark: {
+            a: '#94A684',
+            b: '#AEC3AE',
+        },
+        
+        neutral: {
+            a: '#E4E4D0',
+            b: '#FFEEF4'
+        }
     }
 }
 
