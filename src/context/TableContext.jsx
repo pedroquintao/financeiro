01401cfd1f0c829381323ed0c1685200c9298a70
pedroquintao <pedro.h.quintao@hotmail.com>
2024-04-10 23:31:09 -0300

    Refatoração nas funções addItem e toggleTableVisibility

--- src/context/TableContext.jsx
+++ src/context/TableContext.jsx
@@ -10,34 +10,50 @@ export const TableContextProvider = ({ children }) => {
     const [itemValue, setItemValue] = useState()
     const [itemStatus, setItemStatus] = useState()
 
-    const addItem = (name, value, status, tableType) => {if(tableType === 'revenue') {
-                                                            setTables(prevState => ({...prevState, revenueTable: [...tables.revenueTable, {name, value, status}]}));
-                                                            console.log(`A linha ${name}, ${value}, ${status} foi adicionada à tabela: %cREVENUE`, 'color: green');
-                                                            return;
-                                                            }
-                                                        if(tableType === 'expense') {
-                                                            setTables(prevState => ({...prevState, expenseTable: [...tables.expenseTable, {name, value, status}]}));
-                                                            console.log(`A linha ${name}, ${value}, ${status} foi adicionada à tabela: %cEXPENSE`, 'color: #b44d4d');
-                                                            return;
-                                                        }
-                                                        else {
-                                                            console.log('Entrada para tipo de recurso está errada!', tableType);
-                                                            return
-                                                      }
-                                                    }
-    const toggleTableVisibility = (tableType) => {if(tableType === 'revenue') {
-                                                        setTableVisibility(prevState => ({...prevState, revenueTableVisibility: !tableVisibility.revenueTableVisibility}));
+    const addItem = (name, value, status, tableType) => {
+
+        
+        if(!name || !value) {
+            const emptyInput = !name && !value ? 'name e value' : !name ? 'name' : !value ? 'value' : '';
+            window.alert(`Preencher o(os) campo(s): ${emptyInput}`);
+            return
+        }
+
+        if(!tableType) {
+            console.error('O tipo de tabela não foi especificado');
+            return
+        }
+
+        if(tableType !== 'revenue' || tableType !== 'expense') {
+            console.error('Tipo de tabela inválido: ', tableType);
+            return
+        }
+
+        const newItem = { name, value, status }
+
+        setTables(prevState => ({...prevState, [`${tableType}Table`]: [...prevState[`${tableType}Table`], newItem]}))
+
+        const color = tableType === 'revenue' ? 'green' : '#b44d4d'
+
+        console.log(`A linha ${name}, ${value}, ${status} foi adicionada à tabela: %c${tableType.toUpperCase()}`, `color: ${color}`)
+    }
+
+    const toggleTableVisibility = (tableType) => { if(!tableType){
+
+        console.log('Entrada para tipo de recurso está errada!', tableType);
+        return
+    }
+        // if(tableType === 'revenue') {
+                                                        setTableVisibility(prevState => ({...prevState, [`${tableType}TableVisibility`]: !prevState[`${tableType}TableVisibility`]}));
                                                         return
                                                   }
-                                                  if(tableType === 'expense') {
-                                                        setTableVisibility(prevState => ({...prevState, expenseTableVisibility: !tableVisibility.expenseTableVisibility}));
-                                                        return
-                                                  }
-                                                  else {
-                                                        console.log('Entrada para tipo de recurso está errada!', tableType);
-                                                        return
-                                                  }
-                                                }
+                                                //   if(tableType === 'expense') {
+                                                //         setTableVisibility(prevState => ({...prevState, expenseTableVisibility: !tableVisibility.expenseTableVisibility}));
+                                                //         return
+                                                //   }
+                                                //   else {
+                                                //   }
+                                                
                                                     
 
     
