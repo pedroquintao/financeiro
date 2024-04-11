6190f37c5955f623703e2b2b3bfdca71dc6b2f5f
pedroquintao <pedro.h.quintao@hotmail.com>
2024-04-10 22:38:09 -0300

    O estado table foi convertivo em tables, para que cada tabela receba seus valores específicos.

--- src/context/TableContext.jsx
+++ src/context/TableContext.jsx
@@ -4,13 +4,27 @@ export const TableContext = createContext(null);
 
 export const TableContextProvider = ({ children }) => {
     
-    const [table, setTable] = useState([])
+    const [tables, setTables] = useState({revenueTable: [], expenseTable: []})
     const [tableVisibility, setTableVisibility] = useState({revenueTableVisibility: false, expenseTableVisibility: false})
     const [itemName, seItemName] = useState()
     const [itemValue, setItemValue] = useState()
     const [itemStatus, setItemStatus] = useState()
 
-    const addItem = (name, value, status) => {setTable([...table, {name, value, status}])}
+    const addItem = (name, value, status, tableType) => {if(tableType === 'revenue') {
+                                                            setTables(prevState => ({...prevState, revenueTable: [...tables.revenueTable, {name, value, status}]}));
+                                                            console.log(`A linha ${name}, ${value}, ${status} foi adicionada à tabela: %cREVENUE`, 'color: green');
+                                                            return;
+                                                            }
+                                                        if(tableType === 'expense') {
+                                                            setTables(prevState => ({...prevState, expenseTable: [...tables.expenseTable, {name, value, status}]}));
+                                                            console.log(`A linha ${name}, ${value}, ${status} foi adicionada à tabela: %cEXPENSE`, 'color: #b44d4d');
+                                                            return;
+                                                        }
+                                                        else {
+                                                            console.log('Entrada para tipo de recurso está errada!', tableType);
+                                                            return
+                                                      }
+                                                    }
     const toggleTableVisibility = (tableType) => {if(tableType === 'revenue') {
                                                         setTableVisibility(prevState => ({...prevState, revenueTableVisibility: !tableVisibility.revenueTableVisibility}));
                                                         return
@@ -32,12 +46,12 @@ export const TableContextProvider = ({ children }) => {
 
 
     const context = {
-                table,
+                tables,
                 tableVisibility,
                 itemName, 
                 itemValue,
                 itemStatus,
-                setTable,
+                setTables,
                 setTableVisibility,
                 toggleTableVisibility,
                 seItemName,
