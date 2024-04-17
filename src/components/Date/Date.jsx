ffcb6ae2318f71e9166f79ef5c5dbb42869c0a2d
pedroquintao <pedro.h.quintao@hotmail.com>
2024-04-17 12:47:13 -0300

    Testes para o sistema de seleção de anos foi iniciado. O metodo de seleção definido, é utilizar um botao para esquerda, que subtrairá 1 do ano atual, e se esse ano nao constar no array ou objeto com todos os anos, deverá ser adicionado um novo ano a esse array ou objeto. Outro botao fara algo semelhante para direita

--- src/components/Date/Date.jsx
+++ src/components/Date/Date.jsx
@@ -1,30 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Date = () => {
 
     const [selectedDate, setSelectedDate] = useState('');
 
-    const [mesSelecionado, setMesSelecionado] = useState('');
+    const [selectedMonth, setSelectedMonth] = useState('');
+    const [selectedYear, setSelectedYear] = useState('2022')
 
-    const meses = [
+    const months = [
       'Janeiro', 'Fevereiro', 'Março', 'Abril',
       'Maio', 'Junho', 'Julho', 'Agosto',
       'Setembro', 'Outubro', 'Novembro', 'Dezembro'
     ];
-  
-    const handleChange = (event) => {
-      setMesSelecionado(event.target.value);
+
+    const years = [
+      '2020', '2021', '2022', '2023','2024','2025','2026'
+    ]
+    
+    // const handleYearChange = (direction) => {
+    //   if(direction !== 'right' && direction !== 'left') {
+    //     console.error('Direção errada!', direction)
+    //   }
+
+    //   const selectedYearIndex = years.indexOf(selectedYear)
+    //   direction === 'right' ? sele
+    // }
+
+    const executaTeste = () => {
+          // setSelectedYear(() => {
+            const newYearInt = parseInt(selectedYear) + 1
+            const newYearString = newYearInt.toString()
+            setSelectedYear(newYearString)
+            console.log('Ano anterior: ', selectedYear, ' novo ano é: ', newYearString)
+          // })
+        }
+
+        useEffect(() => {
+              const selectedYearIndex = years.indexOf(selectedYear)
+              console.log('O ano atual é: ', selectedYear)
+              console.log('O indice é ', selectedYearIndex, ' o correto deveria ser: 4')
+        }, [selectedYear])
+
+    const handleMonthChange = (event) => {
+      setSelectedMonth(event.target.value);
     };
 
     return (<>  
-<select value={mesSelecionado} onChange={handleChange}>
-      <option value="" disabled hidden>Mês</option>
-      {meses.map((mes, index) => (
-        <option key={index} value={mes}>
-          {mes}
-        </option>
-      ))}
-    </select>
+              <select value={selectedMonth} onChange={handleMonthChange}>
+                <option value="" disabled hidden>Mês</option>
+                {months.map((mes, index) => (
+                  <option key={index} value={mes}>
+                    {mes}
+                  </option>
+                ))}
+              </select>
+              <button onClick={() => executaTeste()}>TESTE</button>
             </>)
 }
 
