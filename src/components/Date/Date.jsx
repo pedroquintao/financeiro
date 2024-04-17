d3010d5bf3b3ecd220572d217f8d11e8edd0e963
pedroquintao <pedro.h.quintao@hotmail.com>
2024-04-16 22:15:20 -0300

    Seleçao de mes foi criada, em sequencia devera ser criada a selecao de anos

--- src/components/Date/Date.jsx
+++ src/components/Date/Date.jsx
@@ -1,6 +1,31 @@
-export const Date = () => {
-    return(
-    <>
-        <h1>{"< Março >"}</h1>
-    </>)
+import { useState } from "react";
+
+const Date = () => {
+
+    const [selectedDate, setSelectedDate] = useState('');
+
+    const [mesSelecionado, setMesSelecionado] = useState('');
+
+    const meses = [
+      'Janeiro', 'Fevereiro', 'Março', 'Abril',
+      'Maio', 'Junho', 'Julho', 'Agosto',
+      'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+    ];
+  
+    const handleChange = (event) => {
+      setMesSelecionado(event.target.value);
+    };
+
+    return (<>  
+<select value={mesSelecionado} onChange={handleChange}>
+      <option value="" disabled hidden>Mês</option>
+      {meses.map((mes, index) => (
+        <option key={index} value={mes}>
+          {mes}
+        </option>
+      ))}
+    </select>
+            </>)
 }
+
+export default Date;
