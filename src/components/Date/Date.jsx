28efa87548bf0a311299a632cb4328108202a924
pedroquintao <pedro.h.quintao@hotmail.com>
2024-04-17 22:21:09 -0300

    Foi implementada a funcao de alterar os anos ao clicar em um botao para direita ou esquerda. Ao clicar para esq. = -1 ano, se clicar para a dir. + 1 ano. Sempre que o resultado da soma/subtração do ano for um numero que nao existe no array de anos, automaticamente é criado um novo ano nesse array. Outra for de selecionar o ano atual é por um dropdown select. O que deve ser feito agora é implementar esse array de anos e meses no contexto TableContext

--- src/components/Date/Date.jsx
+++ src/components/Date/Date.jsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-grid-system";
 
 const Date = () => {
 
-    const [selectedDate, setSelectedDate] = useState('');
+  
+  const [selectedMonth, setSelectedMonth] = useState('');
+  const [selectedYear, setSelectedYear] = useState('2022');
+  const [years, setYears] = useState([selectedYear])
+  const [selectedDate, setSelectedDate] = useState('');
 
-    const [selectedMonth, setSelectedMonth] = useState('');
-    const [selectedYear, setSelectedYear] = useState('2022')
+  const Dates = {years: `${years.map((year) => {return year})}:`}
 
     const months = [
       'Janeiro', 'Fevereiro', 'Março', 'Abril',
@@ -13,49 +17,79 @@ const Date = () => {
       'Setembro', 'Outubro', 'Novembro', 'Dezembro'
     ];
 
-    const years = [
-      '2020', '2021', '2022', '2023','2024','2025','2026'
-    ]
-    
-    // const handleYearChange = (direction) => {
-    //   if(direction !== 'right' && direction !== 'left') {
-    //     console.error('Direção errada!', direction)
-    //   }
 
-    //   const selectedYearIndex = years.indexOf(selectedYear)
-    //   direction === 'right' ? sele
-    // }
+    const executaTeste = (direction) => {
 
-    const executaTeste = () => {
-          // setSelectedYear(() => {
-            const newYearInt = parseInt(selectedYear) + 1
-            const newYearString = newYearInt.toString()
-            setSelectedYear(newYearString)
-            console.log('Ano anterior: ', selectedYear, ' novo ano é: ', newYearString)
+            switch(direction) {
+
+              case 'right':
+                const yearPlusOne = parseInt(selectedYear) + 1
+                const yearPlusOneString = yearPlusOne.toString()
+                setSelectedYear(yearPlusOneString)
+
+                if(!years.find((element) => element === yearPlusOneString)){
+                  setYears(prevState => [...prevState, yearPlusOneString])
+                }
+
+                break
+
+              case 'left':
+                const yearMinusOne = parseInt(selectedYear) - 1
+                const yearMinusOneString = yearMinusOne.toString()
+                setSelectedYear(yearMinusOneString)
+
+                if(!years.find((element) => element === yearMinusOneString)){
+                  setYears(prevState => [yearMinusOneString, ...prevState ])
+                }
+
+                break
+
+              default:
+                console.error('Direção errada!', direction)
+                break
+            }
           // })
         }
 
         useEffect(() => {
               const selectedYearIndex = years.indexOf(selectedYear)
               console.log('O ano atual é: ', selectedYear)
-              console.log('O indice é ', selectedYearIndex, ' o correto deveria ser: 4')
-        }, [selectedYear])
+              console.log('Array de Anos: ', years)
+              console.log('O indice do ano atual é', selectedYearIndex)
+        }, [selectedYear, years])
 
     const handleMonthChange = (event) => {
       setSelectedMonth(event.target.value);
     };
 
-    return (<>  
-              <select value={selectedMonth} onChange={handleMonthChange}>
-                <option value="" disabled hidden>Mês</option>
-                {months.map((mes, index) => (
-                  <option key={index} value={mes}>
-                    {mes}
-                  </option>
-                ))}
-              </select>
-              <button onClick={() => executaTeste()}>TESTE</button>
-            </>)
+    return (<Container> 
+              <Row justify="center" align="center">
+                <Col justify="center" align="center">
+                      <button onClick={() => executaTeste('left')}>{'<'}</button>
+                      <select value={selectedYear} onChange={(e) => setSelectedYear(e.target.value)}>
+                        {years.map((year, index) => (
+                          <option key={index} value={year}>
+                            {year}
+                          </option>
+                        ))}
+                      </select>
+                      <button onClick={() => executaTeste('right')}>{'>'}</button>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                      <select value={selectedMonth} onChange={handleMonthChange}>
+                        <option value="" disabled hidden>Mês</option>
+                        {months.map((mes, index) => (
+                          <option key={index} value={mes}>
+                            {mes}
+                          </option>
+                        ))}
+                      </select>
+                </Col>
+              </Row>
+              <button onClick={() => console.table('Dates: ', Dates)}>TESTE</button>
+            </Container>)
 }
 
 export default Date;
